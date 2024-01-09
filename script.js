532d0d3fbe69c47a1d72fcde1668ecd7ab9e424d
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-01-09 21:38:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const clockElement = document.getElementById('clock');
+  clockElement.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // To display the clock immediately upon page load
